525266ec8a099c0bc038308fe74128421ac770f4
stewartdawilson <stewart.wilson98@gmail.com>
2019-10-02 13:31:24 +0100

    Added TileQuery example

--- app/src/main/java/com/example/navigatorz/MainActivity.java
+++ app/src/main/java/com/example/navigatorz/MainActivity.java
@@ -1,135 +1,258 @@
 package com.example.navigatorz;
 
-import android.annotation.SuppressLint;
-import android.location.Location;
-import android.util.Log;
-import android.widget.Toast;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
-// Classes needed to initialize the map
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-// Classes needed to handle location permissions
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
-import java.util.List;
-// Classes needed to add the location engine
-import com.mapbox.android.core.location.LocationEngine;
-import com.mapbox.android.core.location.LocationEngineCallback;
-import com.mapbox.android.core.location.LocationEngineProvider;
-import com.mapbox.android.core.location.LocationEngineRequest;
-import com.mapbox.android.core.location.LocationEngineResult;
-import java.lang.ref.WeakReference;
-// Classes needed to add the location component
+import com.mapbox.api.tilequery.MapboxTilequery;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.List;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
 /**
- * Use the Mapbox Core Library to listen to device location updates
+ * Use the Mapbox Tilequery API to retrieve information about Features on a Vector Tileset. More info about
+ * the Tilequery API can be found at https://www.mapbox.com/api-documentation/#tilequery
  */
 public class MainActivity extends AppCompatActivity implements
-        OnMapReadyCallback, PermissionsListener {
-    // Variables needed to initialize a map
+        OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
+
+    private static final String RESULT_GEOJSON_SOURCE_ID = "RESULT_GEOJSON_SOURCE_ID";
+    private static final String CLICK_CENTER_GEOJSON_SOURCE_ID = "CLICK_CENTER_GEOJSON_SOURCE_ID";
+    private static final String LAYER_ID = "LAYER_ID";
+    private static final String RESULT_ICON_ID = "RESULT_ICON_ID";
+    private static final String CLICK_ICON_ID = "CLICK_ICON_ID";
+    private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
     private MapView mapView;
-    // Variables needed to handle location permissions
-    private PermissionsManager permissionsManager;
-    // Variables needed to add the location engine
-    private LocationEngine locationEngine;
-    private long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
-    private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
-    // Variables needed to listen to location updates
-    private MainActivityLocationCallback callback = new MainActivityLocationCallback(this);
+    private TextView tilequeryResponseTextView;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-// Mapbox access token is configured here. This needs to be called either in your application
-// object or in the same activity which contains the mapview.
-        Mapbox.getInstance(this, getString(R.string.access_token));
+            // Mapbox access token is configured here. This needs to be called either in your application
+            // object or in the same activity which contains the mapview.
+            Mapbox.getInstance(this, getString(R.string.access_token));
 
-// This contains the MapView in XML and needs to be called after the access token is configured.
+        // This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_main);
+
+        tilequeryResponseTextView = findViewById(R.id.tilequery_response_info_textview);
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
     }
 
+    @SuppressWarnings( {"MissingPermission"})
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-        this.mapboxMap = mapboxMap;
-
-        mapboxMap.setStyle(Style.TRAFFIC_NIGHT,
-                new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        enableLocationComponent(style);
-                    }
-                });
+        MainActivity.this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                addClickLayer(style);
+                addResultLayer(style);
+                displayDeviceLocation(style);
+                mapboxMap.addOnMapClickListener(MainActivity.this);
+                Toast.makeText(MainActivity.this, R.string.click_on_map_instruction, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /**
-     * Initialize the Maps SDK's LocationComponent
+     * Add a map layer which will show a marker icon where the map was clicked
+     */
+    private void addClickLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addImage(CLICK_ICON_ID, getBitmapFromVectorDrawable(MainActivity.this, R.drawable.red_marker));
+
+        loadedMapStyle.addSource(new GeoJsonSource(CLICK_CENTER_GEOJSON_SOURCE_ID,
+                FeatureCollection.fromFeatures(new Feature[] {})));
+
+        loadedMapStyle.addLayer(new SymbolLayer("click-layer", CLICK_CENTER_GEOJSON_SOURCE_ID).withProperties(
+                iconImage(CLICK_ICON_ID),
+                iconOffset(new Float[] {0f, -12f}),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true)
+        ));
+    }
+
+    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = (DrawableCompat.wrap(drawable)).mutate();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    /**
+     * Add a map layer which will show marker icons for all of the Tilequery API results
+     */
+    private void addResultLayer(@NonNull Style loadedMapStyle) {
+        // Add the marker image to map
+        loadedMapStyle.addImage(RESULT_ICON_ID, getBitmapFromVectorDrawable(MainActivity.this, R.drawable.blue_marker));
+
+
+        // Retrieve GeoJSON information from the Mapbox Tilequery API
+        loadedMapStyle.addSource(new GeoJsonSource(RESULT_GEOJSON_SOURCE_ID));
+
+        loadedMapStyle.addLayer(new SymbolLayer(LAYER_ID, RESULT_GEOJSON_SOURCE_ID).withProperties(
+                iconImage(RESULT_ICON_ID),
+                iconOffset(new Float[] {0f, -12f}),
+                iconIgnorePlacement(true),
+                iconAllowOverlap(true)
+        ));
+    }
+
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        mapboxMap.getStyle(new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                // Move and display the click center layer's red marker icon to wherever the map was clicked on
+                GeoJsonSource clickLocationSource = style.getSourceAs(CLICK_CENTER_GEOJSON_SOURCE_ID);
+                if (clickLocationSource != null) {
+                    clickLocationSource.setGeoJson(Point.fromLngLat(point.getLongitude(), point.getLatitude()));
+                }
+
+                // Use the map click location to make a Tilequery API call
+                makeTilequeryApiCall(point);
+            }
+        });
+        return true;
+    }
+
+    /**
+     * Use the Java SDK's MapboxTilequery class to build a API request and use the API response
+     *
+     * @param point the center point that the the tilequery will originate from.
+     */
+    private void makeTilequeryApiCall(@NonNull LatLng point) {
+        MapboxTilequery tilequery = MapboxTilequery.builder()
+                .accessToken(getString(R.string.access_token))
+                .mapIds("mapbox.mapbox-streets-v7")
+                .query(Point.fromLngLat(point.getLongitude(), point.getLatitude()))
+                .radius(50)
+                .limit(10)
+                .geometry("polygon")
+                .dedupe(true)
+                .layers("building")
+                .build();
+
+        tilequery.enqueueCall(new Callback<FeatureCollection>() {
+            @Override
+            public void onResponse(Call<FeatureCollection> call, Response<FeatureCollection> response) {
+                if (response.body() != null) {
+                    FeatureCollection responseFeatureCollection = response.body();
+                    tilequeryResponseTextView.setText(responseFeatureCollection.toJson());
+                    mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                            GeoJsonSource resultSource = style.getSourceAs(RESULT_GEOJSON_SOURCE_ID);
+                            if (resultSource != null && responseFeatureCollection.features() != null) {
+                                List<Feature> featureList = responseFeatureCollection.features();
+                                if (featureList.isEmpty()) {
+                                    Toast.makeText(MainActivity.this,
+                                            getString(R.string.no_tilequery_response_features_toast), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    resultSource.setGeoJson(FeatureCollection.fromFeatures(featureList));
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FeatureCollection> call, Throwable throwable) {
+                Log.d("Request failed: %s", throwable.getMessage());
+                Toast.makeText(MainActivity.this, R.string.api_error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Use the Maps SDK's LocationComponent to display the device location on the map
      */
     @SuppressWarnings( {"MissingPermission"})
-    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-// Check if permissions are enabled and if not request
+    private void displayDeviceLocation(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
 
-// Get an instance of the component
+            // Get an instance of the component
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
-// Set the LocationComponent activation options
-            LocationComponentActivationOptions locationComponentActivationOptions =
-                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
-                            .useDefaultLocationEngine(false)
-                            .build();
+            // Activate with options
+            locationComponent.activateLocationComponent(LocationComponentActivationOptions.builder(
+                    this, loadedMapStyle).build());
 
-// Activate with the LocationComponentActivationOptions object
-            locationComponent.activateLocationComponent(locationComponentActivationOptions);
-
-// Enable to make component visible
+            // Enable to make component visible
             locationComponent.setLocationComponentEnabled(true);
 
-// Set the component's camera mode
+            // Set the component's camera mode
             locationComponent.setCameraMode(CameraMode.TRACKING);
-
-// Set the component's render mode
             locationComponent.setRenderMode(RenderMode.COMPASS);
 
-            initLocationEngine();
+            // Zoom the camera into the device's current location
+            mapboxMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(new CameraPosition.Builder()
+                            .zoom(17)
+                            .build()), 2000);
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
         }
     }
 
-    /**
-     * Set up the LocationEngine and the parameters for querying the device's location
-     */
-    @SuppressLint("MissingPermission")
-    private void initLocationEngine() {
-        locationEngine = LocationEngineProvider.getBestLocationEngine(this);
-
-        LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
-                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
-
-        locationEngine.requestLocationUpdates(request, callback, getMainLooper());
-        locationEngine.getLastLocation(callback);
-    }
-
-
-
+    // The following three methods are related to showing the device's location via the LocationComponent
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -142,63 +265,20 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onPermissionResult(boolean granted) {
-        if (granted) {
-            if (mapboxMap.getStyle() != null) {
-                enableLocationComponent(mapboxMap.getStyle());
-            }
+        Style style = mapboxMap.getStyle();
+        if (granted && style != null) {
+            displayDeviceLocation(style);
         } else {
             Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
             finish();
         }
     }
 
-    private static class MainActivityLocationCallback
-            implements LocationEngineCallback<LocationEngineResult> {
-
-        private final WeakReference<MainActivity> activityWeakReference;
-
-        MainActivityLocationCallback(MainActivity activity) {
-            this.activityWeakReference = new WeakReference<>(activity);
-        }
-
-        /**
-         * The LocationEngineCallback interface's method which fires when the device's location has changed.
-         *
-         * @param result the LocationEngineResult object which has the last known location within it.
-         */
-        @Override
-        public void onSuccess(LocationEngineResult result) {
-            MainActivity activity = activityWeakReference.get();
-
-            if (activity != null) {
-                Location location = result.getLastLocation();
-
-                if (location == null) {
-                    return;
-                }
-
-
-// Pass the new location to the Maps SDK's LocationComponent
-                if (activity.mapboxMap != null && result.getLastLocation() != null) {
-                    activity.mapboxMap.getLocationComponent().forceLocationUpdate(result.getLastLocation());
-                }
-            }
-        }
-
-        /**
-         * The LocationEngineCallback interface's method which fires when the device's location can not be captured
-         *
-         * @param exception the exception message
-         */
-        @Override
-        public void onFailure(@NonNull Exception exception) {
-            Log.d("LocationChangeActivity", exception.getLocalizedMessage());
-            MainActivity activity = activityWeakReference.get();
-            if (activity != null) {
-                Toast.makeText(activity, exception.getLocalizedMessage(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
+    // Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     @Override
@@ -208,42 +288,35 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
     protected void onStop() {
         super.onStop();
         mapView.onStop();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-// Prevent leaks
-        if (locationEngine != null) {
-            locationEngine.removeLocationUpdates(callback);
-        }
-        mapView.onDestroy();
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mapboxMap != null) {
+            mapboxMap.removeOnMapClickListener(this);
+        }
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
